a20124cff9239e9859bf7b70a889b191254e2a8f
boratanrikulu <boratanrikulu@gmail.com>
2021-06-13 17:38:06 +0300

    client: remove sdp editing

--- assets/javascript/peer.js
+++ assets/javascript/peer.js
@@ -139,7 +139,6 @@ function connect(stream) {
 				pc.setRemoteDescription(offer)
 				pc.createAnswer().then(answer => {
 					pc.setLocalDescription(answer)
-					offer.sdp = setMediaBitrates(offer.sdp);
 					ws.send(JSON.stringify({
 						event: 'answer',
 						data: JSON.stringify(answer)
@@ -170,45 +169,3 @@ navigator.mediaDevices.getUserMedia({
 		document.getElementById('localVideo').srcObject = stream
 		connect(stream)
 	}).catch(err => console.log(err))
-
-function setMediaBitrates(sdp) {
-	return setMediaBitrate(setMediaBitrate(sdp, "video", 50), "audio", 50);
-}
-
-function setMediaBitrate(sdp, media, bitrate) {
-	var lines = sdp.split("\n");
-	var line = -1;
-	for (var i = 0; i < lines.length; i++) {
-		if (lines[i].indexOf("m=" + media) === 0) {
-			line = i;
-			break;
-		}
-	}
-	if (line === -1) {
-		console.debug("Could not find the m line for", media);
-		return sdp;
-	}
-	console.debug("Found the m line for", media, "at line", line);
-
-	// Pass the m line
-	line++;
-
-	// Skip i and c lines
-	while (lines[line].indexOf("i=") === 0 || lines[line].indexOf("c=") === 0) {
-		line++;
-	}
-
-	// If we're on a b line, replace it
-	if (lines[line].indexOf("b") === 0) {
-		console.debug("Replaced b line at line", line);
-		lines[line] = "b=AS:" + bitrate;
-		return lines.join("\n");
-	}
-
-	// Add a new b line
-	console.debug("Adding new b line before line", line);
-	var newLines = lines.slice(0, line)
-	newLines.push("b=AS:" + bitrate)
-	newLines = newLines.concat(lines.slice(line, lines.length))
-	return newLines.join("\n")
-}
