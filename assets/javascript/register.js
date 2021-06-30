0f8e5316f387b94cd43128fc2f4eebff9b037fef
boratanrikulu <boratanrikulu@gmail.com>
2021-06-30 19:30:49 +0300

    pwa: disable sw for safari

--- assets/javascript/register.js
+++ assets/javascript/register.js
@@ -1,7 +1,17 @@
+var isSafari = window.safari !== undefined
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(registration => {
-    console.log('service worker registered:', registration);
-  }).catch(error => {
-    console.log('service worker error:', error)
-  })
+  if (isSafari) { // if it's safari, disable current service workers and don't start a service worker.
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister()
+      }
+    })
+  } else {
+    navigator.serviceWorker.register("/sw.js").then(registration => {
+      console.log('service worker registered:', registration);
+    }).catch(error => {
+      console.log('service worker error:', error)
+    })
+  }
 }
